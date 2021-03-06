b49fc44f004eec17cdc49d5598ef5d48dd00fb9b
Pedro Oliveira <pedro96h@gmail.com>
2021-03-06 12:49:59 -0300

    criando interfaces e factory de dao

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	public static SellerDao CreateSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	public static DepartmentDao CreateDepartmentDao() {
+		return new DepartmentDaoJDBC();
+	}
+}
