355c7dd6bf1d5225c284aa96b4141b4b7bc96810
Pedro Oliveira <pedro96h@gmail.com>
2021-03-06 17:42:46 -0300

    implementação de insert da classe seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,6 +26,12 @@ public class Main {
 		System.out.println("\n=== TEST 3: seller findAll === ");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller insert === ");
+		Seller newSeller = new Seller(0,"Greg","greg@gmail.com",new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = "+newSeller.getId());
+		
 	}
 
 }
