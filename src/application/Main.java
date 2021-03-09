186b48ac1fba147fc068827382b525f757bd1289
Pedro Oliveira <pedro96h@gmail.com>
2021-03-08 21:14:46 -0300

    insert para classe department

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-import db.DbException;
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -15,41 +15,46 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		SellerDao sellerDao = DaoFactory.CreateSellerDao();
-		
-		System.out.println("=== TEST 1: seller findById === ");
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
-		
-		System.out.println("\n=== TEST 1: seller findByDepartmentId === ");
-		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
-		list.forEach(System.out::println);
-		
-		System.out.println("\n=== TEST 3: seller findAll === ");
-		list = sellerDao.findAll();
-		list.forEach(System.out::println);
-		
+//		SellerDao sellerDao = DaoFactory.CreateSellerDao();
+//		
+//		System.out.println("=== TEST 1: seller findById === ");
+//		Seller seller = sellerDao.findById(3);
+//		System.out.println(seller);
+//		
+//		System.out.println("\n=== TEST 1: seller findByDepartmentId === ");
+//		Department department = new Department(2, null);
+//		List<Seller> list = sellerDao.findByDepartment(department);
+//		list.forEach(System.out::println);
+//		
+//		System.out.println("\n=== TEST 3: seller findAll === ");
+//		list = sellerDao.findAll();
+//		list.forEach(System.out::println);
+//		
 //		System.out.println("\n=== TEST 4: seller insert === ");
 //		Seller newSeller = new Seller(0,"Greg","greg@gmail.com",new Date(),4000.0,department);
 //		sellerDao.insert(newSeller);
 //		System.out.println("Inserted! new id = "+newSeller.getId());
+//		
+//		
+//		System.out.println("\n=== TEST 5: seller update === ");
+//		seller = sellerDao.findById(1);
+//		seller.setName("Martha Waine");
+//		sellerDao.update(seller);
+//		System.out.println("Update completed!");
+//		
+//	
+//		System.out.println("\n=== TEST 6: seller update === ");
+//		System.out.print("Enter id from delete test : ");
+//		int id = sc.nextInt();
+//		sellerDao.deleteById(id);
+//		System.out.println("Delete completed");	
 		
-		
-		System.out.println("\n=== TEST 5: seller update === ");
-		seller = sellerDao.findById(1);
-		seller.setName("Martha Waine");
-		sellerDao.update(seller);
-		System.out.println("Update completed!");
-		
-	
-		System.out.println("\n=== TEST 6: seller update === ");
-		System.out.print("Enter id from delete test : ");
-		int id = sc.nextInt();
-		sellerDao.deleteById(id);
-		System.out.println("Delete completed");	
+		DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
 
 			
+		System.out.println("=== TEST 1: department insert === ");
+		departmentDao.insert(new Department(null, "Jogos"));
+		System.out.println("Insert completed !");
 		sc.close();
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,14 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -16,7 +22,29 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("INSERT INTO department " + "(Name) "
+					+ "VALUES " + "(?) ", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("unexpected error! No rows aaffected!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
